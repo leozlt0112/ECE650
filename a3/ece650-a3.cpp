f7853176d0a166e168b21572b4ce919c54869f53
Leo Zhang <l536zhan@uwaterloo.ca>
2023-10-22 18:39:05 -0400

    a3

--- a3/ece650-a3.cpp
+++ a3/ece650-a3.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <iostream>
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main(int argc, char **argv)
+{
 
-
-int main (int argc, char **argv) {
     std::cout << "Replace with your solution for the main "
               << "driver program of Assignment 3\n";
     return 0;
