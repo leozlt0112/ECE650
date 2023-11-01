1ed146c6e9628c8935d2305f08bdf38cf5c21875
Leo Zhang <l536zhan@uwaterloo.ca>
2023-11-01 14:19:45 -0400

    a3

--- a3/ece650-a3.cpp
+++ a3/ece650-a3.cpp
@@ -1,69 +1,108 @@
 #include <vector>
-
 #include <iostream>
 #include <signal.h>
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/wait.h>
-// int procB(char **argv)
-//{
-
-//    argv[0] = (char *)"./ece650-a1.py";
-//    argv[1] = nullptr;
-//    argv[2] = nullptr;
-
-//    execvp("./ece650-a1.py", argv);
-
-//    return 0;
-//}
-// int procA(int argc, char **argv)
-//{
-//    argv[0] = (char *)"regen";
-//    execvp("./regen", argv);
-//    return 0;
-//}
+pid_t child_pid = -1;
+pid_t child_pid2 = -1;
+pid_t child_pid3 = -1;
+void sigchld_handler(int signo)
+{
+    pid_t pid;
+    int status;
+    // Check which child process has exited
+    if ((pid = waitpid(child_pid, &status, WNOHANG)) > 0)
+    {
+        if (child_pid2 != -1)
+            kill(child_pid2, SIGTERM); // Send SIGTERM to the other child process
+        if (child_pid3 != -1)
+            kill(child_pid3, SIGTERM);
+    }
+    else if ((pid = waitpid(child_pid2, &status, WNOHANG)) > 0)
+    {
+        if (child_pid3 != -1)
+            kill(child_pid3, SIGTERM); // Send SIGTERM to the other child process
+        if (child_pid != -1)
+            kill(child_pid, SIGTERM);
+    }
+    else if ((pid = waitpid(child_pid3, &status, WNOHANG)) > 0)
+    {
+        if (child_pid2 != -1)
+            kill(child_pid2, SIGTERM); // Send SIGTERM to the other child process
+        if (child_pid != -1)
+            kill(child_pid, SIGTERM);
+    }
+    exit(1);
+}
 int main(int argc, char **argv)
 {
+    // Install the SIGCHLD handler https:https://en.wikipedia.org/wiki/Sigaction
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    if (sigaction(SIGCHLD, &sa, 0) == -1)
+    {
+        perror(0);
+        exit(1);
+    }
     int pipe1[2]; // regen to a1;
+
+    int bigpipe[2]; // driver to c++
+    //                 // python to c++
     pipe(pipe1);
+    pipe(bigpipe);
     std::vector<pid_t> kids;
-    pid_t child_pid;
     child_pid = fork();
+    // regen pipe
     if (child_pid == 0)
     {
         dup2(pipe1[1], STDOUT_FILENO);
         close(pipe1[0]);
         close(pipe1[1]);
-        argv[0] = (char *)"regen";
+        close(bigpipe[0]);
+        close(bigpipe[1]);
+        argv[0] = (char *)"./regen";
         execvp("./regen", argv);
-        // std::cout << "add \"fzri\" (-12,4) (-14,-13) (7,14) (5,-9) (-7,6)" << std::endl;
-        // std::cout << "add \"yezo\" (6,-14) (7,-15) (14,-5) (-19,-10) (20,-5)" << std::endl;
-        // std::cout << "gg"
-        //           << "\n";
-        // exit(0);
-        // return procA(argc, argv);
     }
     kids.push_back(child_pid);
-    pid_t child_pid2 = fork();
+    child_pid2 = fork();
+    // child 1 python
     if (child_pid2 == 0)
     {
         dup2(pipe1[0], STDIN_FILENO);
         close(pipe1[0]);
         close(pipe1[1]);
-        // return procB(argv);
-        // execl("./ece650-a1.py", "./ece650-a1.py", NULL);
-        while (!std::cin.eof())
-        {
-            std::string input_line;
-            std::getline(std::cin, input_line);
-            std::cout << input_line << std::endl;
-        }
+        close(bigpipe[0]);
+        dup2(bigpipe[1], STDOUT_FILENO);
+        close(bigpipe[1]);
+        execl("./ece650-a1.py", "./ece650-a1.py", NULL);
     }
     kids.push_back(child_pid2);
+    child_pid3 = fork();
+    // python to c++
+    if (child_pid3 == 0)
+    {
+
+        dup2(bigpipe[0], STDIN_FILENO);
+        close(bigpipe[0]);
+        close(pipe1[0]);
+        close(bigpipe[1]);
+        close(pipe1[1]);
+        execl("./ece650-a2", "./ece650-a2", NULL);
+    }
+    kids.push_back(child_pid3);
     close(pipe1[0]);
     close(pipe1[1]);
-    sleep(30);
+    close(bigpipe[0]);
+    dup2(bigpipe[1], STDOUT_FILENO);
+    close(bigpipe[1]);
+    std::string input_line;
+    while (std::getline(std::cin, input_line))
+    {
+        std::cout << input_line << std::endl;
+    }
     for (pid_t k : kids)
     {
         int status;
